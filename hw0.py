b36dc36eae2f0ad00b393835322cbceaa487aa3a
jmakk123 <jaredmaksoud@gmail.com>
2025-03-27 14:34:28 -0500

    updates1

--- hw0.py
+++ hw0.py
@@ -1,33 +1,23 @@
-"""
-Introductory programming assignment.
-"""
 import pandas as pd
 
-"""
-!!!TODO!!!
-"""
 def read_file(filename):
 	'''Input: filename (location of stock.dat)
 	   Output: a list of tuples (ticker, name)
 	'''
 
-	df = pd.read_csv(filename, delimiter=' - ', names=['ticker', 'name'])
+	df = pd.read_csv(filename, delimiter=' - ', names=['ticker', 'name']).itertuples(index=False, name=None)
 
-	return list(df.itertuples(index=False))
+	return df
 
-"""
-!!!TODO!!!
-"""
 def write_csv(parsed, outfile):
 	'''Input: a list of tuples (ticker,name), output location
 	   Output: None/Void, writes a file
 	'''
 
-	parse_df = pd.DataFrame(parsed, columns=['ticker', 'name'])
+	parse_df = pd.DataFrame(parsed, columns=['ticker', 'name']).set_index('ticker').to_csv(outfile)
 	df_csv = parse_df.to_csv(outfile, index='ticker')
 
 	return df_csv
 
 if __name__ == "__main__": 
-    data = read_file("stocks.dat")
-    write_csv(data, "stocks_output.csv")
+    write_csv(read_file('stocks.dat'), "output.csv")
